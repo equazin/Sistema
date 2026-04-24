5789251a8db6628e6082c97be7c33f9d53e7fc9b
Nicolas Benitez <nicopbenitez84@gmail.com>
2026-04-24 08:30:18 -0300

    chore: remove generated/temp files from tracking

--- test2.cjs
+++
@@ -1,7 +0,0 @@
-const e = require('electron');
-console.log('type of e:', typeof e);
-if (typeof e === 'object') {
-  console.log('keys:', Object.keys(e).slice(0, 20));
-  console.log('has app:', 'app' in e);
-  console.log('app:', e.app);
-}
